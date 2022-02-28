4f498432ebbad912e83acff2788cf7a047663abd
Romelianism <Romelianism@gmail.com>
2022-02-28 20:50:07 +0800

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
   reactStrictMode: true,
-}
+});
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const url = "https://fluffy-blog.vercel.app/";
 const title = "Fluffy's Awesome Blog About Him Self";
-const description =
-  "See Some Of Fluffy's Awesome Adventures";
+const description = "See Some Of Fluffy's Awesome Adventures";
 
 const Home: NextPage = () => {
   return (
@@ -14,8 +13,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Fluffy Blog</title>
         <meta name="description" content={title} />
-        <link rel="icon" href="/favicon.ico" />
-
+        <link rel="icon" href="/Fluff.jpg" />
 
         <meta property="fb:app_id" content="377261767145225" />
 
@@ -30,39 +28,199 @@ const Home: NextPage = () => {
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="@romelianism" />
         <meta name="twitter:description" content={description} />
+
+        <link rel="manifest" href="/manifest.json" />
+
+        <link rel="apple-touch-icon" href="icon/apple-icon-180.png" />
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-2048-2732.jpg"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-2732-2048.jpg"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-1668-2388.jpg"
+          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-2388-1668.jpg"
+          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-1536-2048.jpg"
+          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-2048-1536.jpg"
+          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-1668-2224.jpg"
+          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-2224-1668.jpg"
+          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-1620-2160.jpg"
+          media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-2160-1620.jpg"
+          media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-1284-2778.jpg"
+          media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-2778-1284.jpg"
+          media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-1170-2532.jpg"
+          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-2532-1170.jpg"
+          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-1125-2436.jpg"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-2436-1125.jpg"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-1242-2688.jpg"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-2688-1242.jpg"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-828-1792.jpg"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-1792-828.jpg"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-1242-2208.jpg"
+          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-2208-1242.jpg"
+          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-750-1334.jpg"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-1334-750.jpg"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-640-1136.jpg"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icon/apple-splash-1136-640.jpg"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Fluffy&apos;s Blog!<br/>
+          Welcome to Fluffy&apos;s Blog!
+          <br />
           World&apos;s Best Polar Bear!
         </h1>
         <figure>
-          <Image src="/Normal Fluffy.jpg" alt='Fluffy World&apos;s Best Polar Bear!' width={500} height={500}/>
+          <Image
+            src="/Normal Fluffy.jpg"
+            alt="Fluffy World's Best Polar Bear!"
+            width={500}
+            height={500}
+          />
         </figure>
         <p className={styles.description}>
           Fluffy is so great that he saved the world 5 times <br />
           Examples:
           <ol>
             <li className={styles.card}>
-              He singlehandedly started the Green Revolution, preventing what would otherwise have been the <b>largest mass starvation in human history</b>.
+              He singlehandedly started the Green Revolution, preventing what
+              would otherwise have been the{" "}
+              <b>largest mass starvation in human history</b>.
             </li>
             <li className={styles.card}>
-              When he was working in the soviet union as a lieutenant colonel of the Soviet Air Defence Forces he singlehandedly stopped a nuclear false alarm by doing so preventing what would otherwise <b>billions of deaths</b>.
+              When he was working in the soviet union as a lieutenant colonel of
+              the Soviet Air Defence Forces he singlehandedly stopped a nuclear
+              false alarm by doing so preventing what would otherwise{" "}
+              <b>billions of deaths</b>.
             </li>
             <li className={styles.card}>
-              When Fluffy Lacks went to american for Henrietta Lacks cancer treatment, he unwittingly made a tremendous contribution to science. <b>HE CURED CANCER!</b>
+              When Fluffy Lacks went to american for Henrietta Lacks cancer
+              treatment, he unwittingly made a tremendous contribution to
+              science. <b>HE CURED CANCER!</b>
             </li>
             <li className={styles.card}>
-              Before he saved the world from Planet X crashing into earth, he donated his blood to every baby in world giving them <b>100% survival rate</b>.
+              Before he saved the world from Planet X crashing into earth, he
+              donated his blood to every baby in world giving them{" "}
+              <b>100% survival rate</b>.
             </li>
             <li className={styles.card}>
-              {new Date().getFullYear()} years ago he descended from being god to be <b>JESUS🙏</b>, by curing the sick, restoring the sight of the blind, making the lame walk, raising the dead back to life, feeding the hungry, and driving away demons from possessed people.
+              {new Date().getFullYear()} years ago he descended from being god
+              to be <b>JESUS🙏</b>, by curing the sick, restoring the sight of
+              the blind, making the lame walk, raising the dead back to life,
+              feeding the hungry, and driving away demons from possessed people.
             </li>
           </ol>
         </p>
         <figure>
-          <Image src="/Computer Fluffy.jpg" alt='Fluffy World&apos;s Best Polar Bear!' width={500} height={500}/>
+          <Image
+            src="/Computer Fluffy.jpg"
+            alt="Fluffy World's Best Polar Bear!"
+            width={500}
+            height={500}
+          />
         </figure>
       </main>
 
@@ -72,14 +230,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <b>Fluffy</b>
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
